f2b00cb1a84a84cf13af839453b741af20d6816c
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2025-02-03 14:32:56 -0700

    Add password field to User model and implement login validation

--- video-sentiment-saas/src/server/auth/config.ts
+++ video-sentiment-saas/src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { loginSchema } from "~/schemas/auth";
 
 import { db } from "~/server/db";
 
@@ -40,7 +41,19 @@ export const authConfig = {
       credentials: {
         email: {label: "Email", type: "email"},
         password: {label: "Password", type: "password"},
-      }
+      },
+      async authorize(credentials) {
+        try{
+          const { email, password } = await loginSchema.parse(credentials);
+
+          const user = await db.user.findUnique({
+            where: { email },
+          });
+          if (!user || !user.pa)
+        } catch (error) {
+          return null;
+        }
+      },
     }),
     /**
      * ...add more providers here.
